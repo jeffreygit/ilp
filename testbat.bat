33c8d280ae6a175b873a594d040c3c819f283051
Jeffrey Kiezebrink <jeffrey.kiezebrink@strukton.com>
2016-09-01 14:34:08 +0200

    wip: wip on feature branch

--- testbat.bat
+++
@@ -1,3 +0,0 @@
-for i in {0..20} ; do                                            
-  { time git ls-remote git@gitlab.com:gitlab-org/gitlab-development-kit.git ; } 2>&1
-done | awk 'BEGIN {total=0 ; max=0 ; min=1} /^git/ {if($10 > max) max = $10} {if ($10 != "" && $10 < min) min=$10} {total+=$10} END {print "total " total " avg " total/20 " min " min " max " max}'
